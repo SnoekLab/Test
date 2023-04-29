29f9f1c72dbcfceb61c203d32bf39b4e0b6be685
SnoekLab <132074236+SnoekLab@users.noreply.github.com>
2023-04-29 15:42:22 +0200

    Step 5

--- GGplot_test.R
+++ GGplot_test.R
@@ -5,11 +5,17 @@
 
 library(ggplot2)
 
-datasets::airquality
-ggplot(datasets::airquality,aes(Day,Temp))+
+use.data <- datasets::airquality
+Values <- c(use.data$Ozone,use.data$Solar.R,use.data$Wind,use.data$Temp)
+Month <- use.data$Month
+Day <- use.data$Day
+Parameter <- rep(c("Ozone","Solar rad","Wind","Temp"),each=nrow(use.data))
+to.pl <- data.frame(Month,Day,Values,Parameter)
+
+ggplot(to.pl,aes(Day,Values))+
   geom_line()+
   geom_smooth()+
-  facet_grid(.~Month,scale="free_x",space="free_x")
+  facet_grid(Parameter~Month,scale="free",space="free_x")
 
 
 
