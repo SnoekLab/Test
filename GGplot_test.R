1a9a3babc01f3136776baaaabb0cb9c89f8276bd
SnoekLab <132074236+SnoekLab@users.noreply.github.com>
2023-04-29 14:50:49 +0200

    Step4

--- GGplot_test.R
+++ GGplot_test.R
@@ -5,7 +5,11 @@
 
 library(ggplot2)
 
-
+datasets::airquality
+ggplot(datasets::airquality,aes(Day,Temp))+
+  geom_line()+
+  geom_smooth()+
+  facet_grid(.~Month,scale="free_x",space="free_x")
 
 
 
